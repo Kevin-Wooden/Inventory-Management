76c6955ab614634534cedc96253b20dc770cf010
Kevin Wooden <77079277+Kevin-Wooden@users.noreply.github.com>
2021-04-05 11:05:22 -0700

    Update Inv.h

--- Inv.h
+++ Inv.h
@@ -1,2 +1,10 @@
-#define Starting_MSG "We have %d watermelons in Stock today\n", StartingInv
+#define Starting_MSG "We have %d Watermelons in Stock today\n", StartingWatermelon
+#define Starting_MSG1 "We have %d Kiwis in Stock today\n", StartingKiwi
+#define Starting_MSG2 "We have %d Strawberries in Stock today\n", StartingStrawberry
+#define Starting_MSG3 "We have %d Pineapples in Stock today\n", StartingPineapple
+#define Starting_MSG4 "We have %d Blueberries in Stock today\n", StartingBlueberry
 #define Sold_MSG "Input how many watermelons were sold: \n"
+#define Sold_MSG1 "Input how many Kiwis were sold: \n"
+#define Sold_MSG2 "Input how many Strawberries were sold: \n"
+#define Sold_MSG3 "Input how many Pineapples were sold: \n"
+#define Sold_MSG4 "Input how many Blueberries were sold: \n"
